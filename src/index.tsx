56f5bfa4db7249f7330685b3e396d9266699ac57
二货机器人 <smith3816@gmail.com>
2024-12-24 15:36:13 +0800

    chore: fix race

--- src/index.tsx
+++ src/index.tsx
@@ -1,1 +1,13 @@
+import { unstableSetRender } from 'antd';
 import { createRoot } from 'react-dom/client';
+
+unstableSetRender((node, container) => {
+  (container as any)._reactRoot ||= createRoot(container);
+  const root: ReturnType<typeof createRoot> = (container as any)._reactRoot;
+  root.render(node);
+  return async () => {
+    // Wait for timeout to avoid React render race.
+    await new Promise((resolve) => setTimeout(resolve, 0));
+    root.unmount();
+  };
+});
